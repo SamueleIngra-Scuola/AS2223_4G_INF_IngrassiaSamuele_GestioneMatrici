4d5ec8049e8790f4c64227d546fa2a2b555c06d1
S4pphiree <s4pph1r3sam@gmail.com>
2022-10-26 23:11:59 +0200

    Everything's Done!

--- MatrixManager.cs
+++ MatrixManager.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics.Metrics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -36,9 +37,14 @@ namespace AS2223_4G_INF_IngrassiaSamuele_GestioneMatrici
         /// </summary>
         /// <param name="minValue"></param>
         /// <param name="maxValue"></param>
-        public void LoadMatrix(int minValue, int maxValue)
+        public void FillMatrix(int minValue, int maxValue)
         {
-
+            Random random = new System.Random();
+            for (int i = 0; i < matSize; i++)
+                for(int j = 0; j < matSize; j++)
+                {
+                    mat[i, j] = random.Next(minValue, maxValue);
+                }
         }
 
         /// <summary>
@@ -46,7 +52,14 @@ namespace AS2223_4G_INF_IngrassiaSamuele_GestioneMatrici
         /// </summary>
         public void PrintMatrix()
         {
-
+            for (int i = 0; i < matSize; i++)
+            {
+                for (int j = 0; j < matSize; j++)
+                {
+                    Console.Write($"{mat[i, j]}\t");
+                }
+                Console.Write("\n");
+            }
         }
 
         /// <summary>
@@ -54,7 +67,20 @@ namespace AS2223_4G_INF_IngrassiaSamuele_GestioneMatrici
         /// </summary>
         public void PrintMatrixFrame()
         {
-
+            int i;
+            
+            for (i = 0; i < matSize; i++) //Left Column
+                Console.Write($"{mat[i, 0]}\t");
+            Console.Write("\n");
+            for (i = 1; i < matSize; i++) //Bottom Row
+                Console.Write($"{mat[matSize - 1, i]}\t");
+            Console.Write("\n");
+            for (i = matSize - 2; i >= 0; i--) //Right Column
+                Console.Write($"{mat[i, matSize - 1]}\t");
+            Console.Write("\n");
+            for (i = matSize - 2; i > 0; i--) //Top Row
+                Console.Write($"{mat[0, i]} \t");
+            Console.Write("\n");
         }
         
         /// <summary>
@@ -62,7 +88,12 @@ namespace AS2223_4G_INF_IngrassiaSamuele_GestioneMatrici
         /// </summary>
         public void InvertFirstLastColumn()
         {
-
+            for (int i = 0; i < matSize; i++)
+            {
+                int temp = mat[i, 0];
+                mat[i, 0] = mat[i, matSize - 1];
+                mat[i, matSize - 1] = temp;
+            }
         }
         
         /// <summary>
@@ -73,6 +104,13 @@ namespace AS2223_4G_INF_IngrassiaSamuele_GestioneMatrici
         public int NumberCounter(int searchedNumber)
         {
             int nCounter = 0;
+
+            for (int i = 0; i < matSize; i++)
+                for (int j = 0; j < matSize; j++)
+                {
+                    if (searchedNumber == mat[i, j])
+                        nCounter++;
+                }
 
             return nCounter;
         }
@@ -86,8 +124,37 @@ namespace AS2223_4G_INF_IngrassiaSamuele_GestioneMatrici
         public double CalcMinMaxAverage(ref int min, ref int max)
         {
             double average = 0;
+            double sum = 0;
+            int nValues = matSize * matSize;
+            
+            min = mat[0, 0];
+            max = mat[0, 0];
+            
+            for (int i = 0; i < matSize; i++)
+                for (int j = 0; j < matSize; j++)
+                {
+                    if (mat[i, j] < min) 
+                        min = mat[i, j];
+                    if (mat[i, j] > max) 
+                        max = mat[i, j];
+                    
+                    sum += mat[i, j];
+                }
+
+            average = sum / nValues;
 
             return average;
+        }
+
+        /// <summary>
+        /// Returns the value of a cell of the matrix
+        /// </summary>
+        /// <param name="row"></param>
+        /// <param name="column"></param>
+        /// <returns></returns>
+        public double GetValue(int row, int column)
+        {
+            return mat[row, column];
         }
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,13 +1,48 @@
 ﻿using AS2223_4G_INF_IngrassiaSamuele_GestioneMatrici;
+using System;
 
 const int MIN_VALUE_MAT = -100;
 const int MAX_VALUE_MAT = 75;
 
-Console.WriteLine("Inserisci la dimensione del lato della matrice: ");
+Console.WriteLine("Request #1\n");
+
+Console.Write("Inserisci la dimensione del lato della matrice: ");
 int matSize = Int32.Parse(Console.ReadLine());
 
-MatrixManager GM = new MatrixManager(matSize;
+MatrixManager GM = new MatrixManager(matSize);
 
+GM.FillMatrix(MIN_VALUE_MAT, MAX_VALUE_MAT);
+Console.WriteLine("Matrice Riempita!");
+
+Console.WriteLine("\nRequest #2\n");
+
+GM.PrintMatrix();
+
+Console.WriteLine("\nRequest #3\n");
+
+Console.Write("Inserisci il valore da ricercare: ");
+int searchedValue = Int32.Parse(Console.ReadLine());
+Console.WriteLine($"Sono state trovate {GM.NumberCounter(searchedValue)} celle contenenti il numero {searchedValue}!");
+
+Console.WriteLine("\nRequest #4\n");
+
+GM.PrintMatrixFrame();
+
+Console.WriteLine("\nRequest #5\n");
+
+GM.PrintMatrix();
+GM.InvertFirstLastColumn();
+Console.WriteLine("\nColonne Invertite!\n");
+GM.PrintMatrix();
+
+Console.WriteLine("\nRequest #6");
+
+int min = 0, max = 0;
+double average;
+
+average = GM.CalcMinMaxAverage(ref min, ref max);
+
+Console.WriteLine($"Valore Massimo: {max} | Valore Minimo: {min} | Media: {average}");
 
 
 
